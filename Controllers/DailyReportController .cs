80d1f3ba3f385ceb291a1dd6346e04313398a5f7
gpfernando2024 <gpfernandoribeiro@gmail.com>
2024-07-25 19:26:57 -0300

    Atualizações inclusão do campo Observação

--- Controllers/DailyReportController .cs
+++ Controllers/DailyReportController .cs
@@ -20,9 +20,11 @@ using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.WebControls;
 using NPOI.SS.Formula.Functions;
 using System.Drawing;
+using GestaoDemandas.Enumeradores;
 
 /*
     Campos DevOps Analytics
+
     Custom_22fc3f0b__002D6c54__002D4770__002Dacb3__002D8d7b813ae13a = Data Real da Homologação
     Custom_768b8fc1__002D37ad__002D4ebb__002Da7e1__002Df8f7bc8e2c1c = GerênciaProdesp
     Custom_b4f03334__002D2822__002D4015__002D8439__002D3f002a94bf8e = DescriçãoProjeto (Descrição do Projeto, relacionada a sessão Gestão Prodesp)
@@ -48,7 +50,11 @@ using System.Drawing;
     Custom_DataInicioPrevisto = DataInicioPrevisto
     Custom_GerenteProjeto = GerenteProjeto
 
-    https://analytics.dev.azure.com/devopssee/CFIEE%20-%20Coordenadoria%20de%20Finan%C3%A7as%20e%20Infra%20Estrutura%20Escolar/_odata/v3.0-preview/$metadata#WorkItems(WorkItemId,Title,State,Custom_Sistema,Custom_Prioridade_Epic,Custom_Finalidade,Custom_NomeProjeto,Custom_GerenteProjeto,Custom_b4f03334__002D2822__002D4015__002D8439__002D3f002a94bf8e,CreatedDate,Custom_DataInicioAtendimento,Custom_DataPrevistaDaEntrega,Custom_c4b5f670__002D39f1__002D40fd__002Dace5__002D329f6170c36d,Custom_e9e5e387__002D39de__002D4875__002D94a5__002Db5721f8e21ef
+    URL:https://analytics.dev.azure.com/devopssee/CFIEE%20-%20Coordenadoria%20de%20Finan%C3%A7as%20e%20Infra%20Estrutura%20Escolar/_odata/v3.0-preview/WorkItems?
+        $filter=(indexof(Custom_Sistema, 'Transporte Escolar') ge 0 or indexof(Custom_Sistema, 'Indicação Escolas PEI') ge 0 or indexof(Custom_Sistema, 'PLACON') ge 0) and WorkItemType eq 'User Story'
+        &$select=WorkItemId,Title,State,Custom_Sistema,Custom_Prioridade_Epic,Custom_Finalidade,Custom_NomeProjeto,Custom_SemanaProdesp,Custom_EntregaValor,Custom_dd460af2__002D5f88__002D4581__002D8205__002De63c777ecef9,Custom_b4f03334__002D2822__002D4015__002D8439__002D3f002a94bf8e,Custom_b4f03334__002D2822__002D4015__002D8439__002D3f002a94bf8e,CreatedDate,Custom_DataInicioAtendimento,Custom_DataPrevistaDaEntrega,Custom_c4b5f670__002D39f1__002D40fd__002Dace5__002D329f6170c36d,Custom_e9e5e387__002D39de__002D4875__002D94a5__002Db5721f8e21ef
+        &$orderby=CreatedDate desc
+
  */
 
 namespace GestaoDemandas.Controllers
@@ -56,10 +62,11 @@ namespace GestaoDemandas.Controllers
       
     public class DailyReportController : Controller
     {
-        private static readonly string AzureAnalyticsUrl = "https://analytics.dev.azure.com/devopssee/CFIEE%20-%20Coordenadoria%20de%20Finanças%20e%20Infra%20Estrutura%20Escolar/_odata/v3.0-preview/WorkItems?$filter=(indexof(Custom_Sistema, 'Transporte Escolar') ge 0 or indexof(Custom_Sistema, 'Indicação Escolas PEI') ge 0 or indexof(Custom_Sistema, 'PLACON') ge 0) and WorkItemType eq 'User Story'&$select=WorkItemId,Title,State,Custom_Sistema,Custom_Prioridade_Epic,Custom_Finalidade,Custom_NomeProjeto,Custom_SemanaProdesp,Custom_EntregaValor,Custom_dd460af2__002D5f88__002D4581__002D8205__002De63c777ecef9,Custom_b4f03334__002D2822__002D4015__002D8439__002D3f002a94bf8e,Custom_b4f03334__002D2822__002D4015__002D8439__002D3f002a94bf8e,CreatedDate,Custom_DataInicioAtendimento,Custom_DataPrevistaDaEntrega,Custom_c4b5f670__002D39f1__002D40fd__002Dace5__002D329f6170c36d,Custom_e9e5e387__002D39de__002D4875__002D94a5__002Db5721f8e21ef&orderby=WorkItemId desc";
+        private static readonly string AzureAnalyticsUrl = "https://analytics.dev.azure.com/devopssee/CFIEE%20-%20Coordenadoria%20de%20Finan%C3%A7as%20e%20Infra%20Estrutura%20Escolar/_odata/v3.0-preview/WorkItems?\r\n        $filter=(indexof(Custom_Sistema, 'Transporte Escolar') ge 0 or indexof(Custom_Sistema, 'Indicação Escolas PEI') ge 0 or indexof(Custom_Sistema, 'PLACON') ge 0) and WorkItemType eq 'User Story'\r\n        &$select=WorkItemId,Custom_Atividade,Title,State,Custom_Sistema,Custom_Prioridade_Epic,Custom_Finalidade,Custom_NomeProjeto,Custom_SemanaProdesp,Custom_EntregaValor,Custom_dd460af2__002D5f88__002D4581__002D8205__002De63c777ecef9,Custom_b4f03334__002D2822__002D4015__002D8439__002D3f002a94bf8e,Custom_b4f03334__002D2822__002D4015__002D8439__002D3f002a94bf8e,CreatedDate,Custom_DataInicioAtendimento,Custom_DataPrevistaDaEntrega,Custom_c4b5f670__002D39f1__002D40fd__002Dace5__002D329f6170c36d,Custom_e9e5e387__002D39de__002D4875__002D94a5__002Db5721f8e21ef\r\n&$orderby=CreatedDate desc";
 
         // Token de autorização (substitua com seu token real - Data da Expiração: 31/12/2024)
         private static readonly string AuthToken = "3icjlaewk56pj3v2d6z5ssarsfjwojd2uy54mupc4mavje2ijewq";
+        private SituacaoAtividade Status;
 
         public async Task<ActionResult> StatusReport()
         {
@@ -132,12 +139,6 @@ namespace GestaoDemandas.Controllers
 
                     // Projetos Suspenso e Abortados
                     AddSuspendProjects(doc, model.projectSuspendItems);
-
-                    // Adiciona a seção Custom_Sistema nos Eventos e Entregas
-                    //AddCustomSistemaEventDelivery(doc, model.EventsDeliveries);
-
-                    // Adiciona a seção Custom_Sistema nos Projetos em Andamento
-                    //AddCustomSistemaOngoingProject(doc, model.OngoingProjects);
 
                     // Escreve o documento no stream de memória
                     doc.Write(ms);
@@ -247,7 +248,7 @@ namespace GestaoDemandas.Controllers
                         DataInicioAtendimento = GetNullableDateTime(item, "Custom_DataInicioAtendimento"),
                         DataPrevistaEntrega = GetNullableDateTime(item, "Custom_DataPrevistaDaEntrega"),
                         Status = state,
-                        Observacao = item.Custom_Finalidade,
+                        Observacao = ObservacaoHelper.ObterObservacao(Status),
                         Conclusao = GetNullableDateTime(item, "Custom_e9e5e387__002D39de__002D4875__002D94a5__002Db5721f8e21ef"), // Data Fechamento
                         DescriçãoProjeto = item.Custom_b4f03334__002D2822__002D4015__002D8439__002D3f002a94bf8e,
                         GerênciaProdesp = item.Custom_768b8fc1__002D37ad__002D4ebb__002Da7e1__002Df8f7bc8e2c1c,
@@ -328,7 +329,7 @@ namespace GestaoDemandas.Controllers
                         DataInicioAtendimento = GetNullableDateTime(item, "Custom_DataInicioAtendimento"),
                         DataPrevistaEntrega = GetNullableDateTime(item, "Custom_DataPrevistaDaEntrega"),
                         Status = state,
-                        Observacao = item.Custom_Finalidade,
+                        Observacao = ObservacaoHelper.ObterObservacao(Status),
                         Conclusao = GetNullableDateTime(item, "Custom_e9e5e387__002D39de__002D4875__002D94a5__002Db5721f8e21ef"), // Data Fechamento
                         DescriçãoProjeto = item.Custom_b4f03334__002D2822__002D4015__002D8439__002D3f002a94bf8e,
                         GerênciaProdesp = item.Custom_768b8fc1__002D37ad__002D4ebb__002Da7e1__002Df8f7bc8e2c1c,
@@ -336,6 +337,7 @@ namespace GestaoDemandas.Controllers
                         SemanaProdesp = item.Custom_SemanaProdesp,
                         NomeProjeto = item.Custom_NomeProjeto,
                         DataRealHomologação = GetNullableDateTime(item, "Custom_22fc3f0b__002D6c54__002D4770__002Dacb3__002D8d7b813ae13a"), // Data Real da Homologação
+                        
                     };
 
                     // Convertendo datas com tratamento para evitar exceções
@@ -363,6 +365,11 @@ namespace GestaoDemandas.Controllers
                     items.Add(projectItem);
                 }
             }
+            // Ordenar os projetos pelo sistema e pela data de abertura
+            var orderedItems = items
+                .OrderBy(p => p.Custom_Sistema) // Ordena pelo sistema (ordem alfabética)
+                .ThenBy(p => p.DataAbertura)   // Em seguida, ordena pela data de abertura
+                .ToList();
             return items;
         }
 
@@ -389,7 +396,7 @@ namespace GestaoDemandas.Controllers
                         DataInicioAtendimento = GetNullableDateTime(item, "Custom_DataInicioAtendimento"),
                         DataPrevistaEntrega = GetNullableDateTime(item, "Custom_DataPrevistaDaEntrega"),
                         Status = state,
-                        Observacao = item.Custom_Finalidade,
+                        Observacao = ObservacaoHelper.ObterObservacao(Status),
                         Conclusao = GetNullableDateTime(item, "Custom_e9e5e387__002D39de__002D4875__002D94a5__002Db5721f8e21ef"), // Data Fechamento
                         DescriçãoProjeto = item.Custom_b4f03334__002D2822__002D4015__002D8439__002D3f002a94bf8e,
                         GerênciaProdesp = item.Custom_768b8fc1__002D37ad__002D4ebb__002Da7e1__002Df8f7bc8e2c1c,
@@ -632,11 +639,6 @@ namespace GestaoDemandas.Controllers
                            e.Conclusao.HasValue &&
                            e.Conclusao.Value.Date == today)  // Verifique a data específica de conclusão
                .GroupBy(e => e.Custom_Sistema);
-            /*
-            var groupedEvents = eventsDeliveries
-                .Where(e => e.Status == "Concluído")
-                .GroupBy(p => p.Custom_Sistema);
-            */
 
             // Verificando se há eventos agrupados
             if (groupedEvents.Any())
@@ -704,6 +706,7 @@ namespace GestaoDemandas.Controllers
                     CreateInfoParagraph(doc, "• Status", item.Status);
                     CreateInfoParagraph(doc, "• Data Conclusão", item.Conclusao != default ? item.Conclusao?.ToString("dd/MM/yyyy") : "N/A");
                     CreateInfoParagraph(doc, "• Observação", item.Observacao);
+                    
 
 
                     // Ajuste de espaço entre parágrafos
@@ -749,7 +752,7 @@ namespace GestaoDemandas.Controllers
             shd.fill = "#B8CCE4"; // Cor azul em hexadecimal
 
             var groupedProjects = ongoingProjects
-               .Where(p => p.Custom_Sistema == "Transporte Escolar") 
+               .Where(p => p.Custom_Sistema == "Transporte Escolar")
                .GroupBy(p => p.Custom_Sistema);
 
             int systemCounter = 1;
---
+++ Enumeradores/Enum.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GestaoDemandas.Enumeradores
+{
+    public enum SituacaoAtividade
+    {
+        /*Status Atividade: 
+         Concluido = 1,             
+         Analise = 2,
+         SuspensoTemp = 3,
+         Desenvolvimento = 4,
+         Aberto = 5,
+         Suspenso = 6,
+         Deploy Producao = 7,
+         Aguardando Solicitante = 8
+         */
+
+        Concluido = 1,             
+        Analise = 2,
+        SuspensoTemp = 3,
+        Desenvolvimento = 4,
+        Aberto = 5,
+        Suspenso = 6,
+        DeployProducao = 7,
+        AguardandoSolicitante = 8
+    }
+
+    public class ObservacaoHelper
+    {
+        public static string ObterObservacao(SituacaoAtividade situacao)
+        {
+            switch (situacao)
+            {
+                case SituacaoAtividade.Concluido:
+                    // Lógica para calcular a data de publicação em produção
+                    DateTime dataPublicacao = DateTime.Now.AddDays(-1);
+                    if (dataPublicacao.DayOfWeek == DayOfWeek.Saturday)
+                        dataPublicacao = dataPublicacao.AddDays(-2);
+                    else if (dataPublicacao.DayOfWeek == DayOfWeek.Sunday)
+                        dataPublicacao = dataPublicacao.AddDays(-3);
+
+                    return $"Publicado em Produção: {dataPublicacao.ToShortDateString()}";
+
+                case SituacaoAtividade.Analise:
+                    return "Em análise";
+
+                case SituacaoAtividade.SuspensoTemp:
+                    return "Atividade suspensa temporariamente para atendimento de outras demandas urgentes ou projeto/atividade suspenso por decisão do cliente";
+
+                case SituacaoAtividade.Desenvolvimento:
+                    return "Atividade em desenvolvimento";
+
+                case SituacaoAtividade.Suspenso:
+                    return "Projeto suspenso e/ou abortado pelo cliente";
+
+                case SituacaoAtividade.AguardandoSolicitante:
+                    return "Atividade no aguardo de homologação do cliente ou outra ação relevante do cliente";
+
+                case SituacaoAtividade.Aberto:
+                    return "Atividade pendente de início";
+
+                default:
+                    return "Situação não reconhecida";
+            }
+        }
+    }
+}
