7f7d7a44fd498068fe36999d672f959d6faa280a
gpfernando2024 <gpfernandoribeiro@gmail.com>
2024-08-29 17:36:55 -0300

    Ajuste dos campos de retorno da URL definindo nome de campos

--- Models/WorkItem.cs
+++ Models/WorkItem.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,12 +15,11 @@ namespace GestaoDemandas.Models
         public string State { get; set; }
         public string Custom_Prioridade_Epic { get; set; }
         public string Custom_Finalidade { get; set; }
-        /*Custom_DataAbertura*/
-        //public DateTime Custom_c4b5f670__002D39f1__002D40fd__002Dace5__002D329f6170c36d { get; set; }
         public WorkItem(DateTime? custom_c4b5f670__002D39f1__002D40fd__002Dace5__002D329f6170c36d, DateTime? Custom_e9e5e387__002D39de__002D4875__002D94a5__002Db5721f8e21ef)
         {
             if (custom_c4b5f670__002D39f1__002D40fd__002Dace5__002D329f6170c36d.HasValue)
             {
+                /*Custom_DataAbertura*/
                 Custom_DataAbertura = custom_c4b5f670__002D39f1__002D40fd__002Dace5__002D329f6170c36d.Value;
             }
             else
@@ -29,6 +29,7 @@ namespace GestaoDemandas.Models
 
             if (Custom_e9e5e387__002D39de__002D4875__002D94a5__002Db5721f8e21ef.HasValue)
             {
+                /*Custom_DataFechamento*/
                 Custom_DataFechamento = Custom_e9e5e387__002D39de__002D4875__002D94a5__002Db5721f8e21ef.Value;
             }
             else
@@ -38,18 +39,33 @@ namespace GestaoDemandas.Models
         }
         public DateTime? Custom_DataInicioAtendimento { get; set; }
         public DateTime? Custom_DataPrevistaDaEntrega { get; set; }
-        /*Custom_DataFechamento*/
-        //public DateTime? Custom_e9e5e387__002D39de__002D4875__002D94a5__002Db5721f8e21ef { get; set; }
-        //DescriçãoProjeto (Descrição do Projeto, relacionada a sessão Gestão Prodesp)
-        public string Custom_b4f03334__002D2822__002D4015__002D8439__002D3f002a94bf8e { get; set; }
-        //GerênciaProdesp
-        public string Custom_768b8fc1__002D37ad__002D4ebb__002Da7e1__002Df8f7bc8e2c1c { get; set; }
-        //Entrega Estratégica
-        public string Custom_dd460af2__002D5f88__002D4581__002D8205__002De63c777ecef9 { get; set; }
+        public string DescricaoProjeto {  get; set; }
         public string Custom_SemanaProdesp { get; set; }
         public string Custom_NomeProjeto { get; set; }
-        //Data Real da Homologação
-        public DateTime? Custom_22fc3f0b__002D6c54__002D4770__002Dacb3__002D8d7b813ae13a { get; set; }
+        public string EntregaEstrategica { get; set; }
+        public string GerenciaProdesp { get; set; }
+        public DateTime ? Custom_DataRealHomologacao {  get; set; }
+
+        [JsonConstructor]
+        public WorkItem(DateTime? Custom_22fc3f0b__002D6c54__002D4770__002Dacb3__002D8d7b813ae13a, 
+             string Custom_b4f03334__002D2822__002D4015__002D8439__002D3f002a94bf8e, 
+             string Custom_dd460af2__002D5f88__002D4581__002D8205__002De63c777ecef9, string Custom_768b8fc1__002D37ad__002D4ebb__002Da7e1__002Df8f7bc8e2c1c)
+        {
+            //Data Real da Homologação
+            if (Custom_22fc3f0b__002D6c54__002D4770__002Dacb3__002D8d7b813ae13a.HasValue)
+            {
+                Custom_DataRealHomologacao = Custom_22fc3f0b__002D6c54__002D4770__002Dacb3__002D8d7b813ae13a.Value;
+            }
+            else
+            {
+                Custom_DataRealHomologacao = null; // ou DateTime.MinValue, se preferir
+            }
+
+            DescricaoProjeto = Custom_b4f03334__002D2822__002D4015__002D8439__002D3f002a94bf8e;  //DescriçãoProjeto (Descrição do Projeto, relacionada a sessão Gestão Prodesp)
+            EntregaEstrategica = Custom_dd460af2__002D5f88__002D4581__002D8205__002De63c777ecef9;//Entrega Estratégica
+            GerenciaProdesp = Custom_768b8fc1__002D37ad__002D4ebb__002Da7e1__002Df8f7bc8e2c1c;   //GerênciaProdesp
+        }
+
         public string Custom_Atividade { get; set; }
         public DateTime? Custom_DataAbertura { get; set; }
         public DateTime? Custom_DataFechamento { get; set; }
